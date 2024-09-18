76916454e6541dda7bc154a3e77dd52b0065aa62
dashluu <dashluu9121997@gmail.com>
2024-09-18 12:13:54 -0700

    refactor: added Python code to show the result of bug fixes.

--- src/pygrad_main.py
+++ src/pygrad_main.py
@@ -1,10 +1,14 @@
+import faulthandler
 from toygrad_cpu import Tensor, Shape, TensorGraph
 
+faulthandler.enable()
 t1 = Tensor.randn([2, 3, 4])
 t2 = Tensor.randn([2, 3, 4])
-t3 = t1 + t2
-graph = TensorGraph.from_tensor(t3)
+t3 = t1.max(1)
+t4 = t3.sum()
+graph = TensorGraph.from_tensor(t4)
 graph.forward()
+graph.backward()
 print(t1)
-print(t2)
 print(t3)
+print(t1.grad())
